49bf44629d0ccd40594a30166a6f3692ac36989c
BijjuXD <103484184+bijju089@users.noreply.github.com>
2025-02-20 19:55:17 +0530

    Set Version

--- template.php
+++ template.php
@@ -23,7 +23,7 @@ class Aurora_Template extends TemplateBase {
     public function __construct($cache, $smarty, $language, $user, $pages) {
         $template = [
             'name' => 'Aurora',
-            'version' => '2.0',
+            'version' => 'dev',
             'nl_version' => '2.1.3',
             'author' => '<a href="https://cxstudios.in/" target="_blank">cxSTUDIOS</a> & <a href="https://github.com/bijju089/aurora-theme/graphs/contributors" target="_blank">Contributors</a>',
         ];
